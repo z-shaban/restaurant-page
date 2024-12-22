907b5b00c8f8bd5a508b614db8b440fe15566266
shaban <146794146+sarzhh@users.noreply.github.com>
2024-12-22 12:20:11 +0000

    import all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("it works");
+import {home} from "./homepage.js";
+import {menu} from "./menu.js";
+import {contact} from "./contact.js";
+
+const homeTab = document.querySelector("#home");
+const menuTab = document.querySelector("#menu");
+const contactTab = document.querySelector("#contact");
+const content = document.querySelector("#content");
+
+homeTab.addEventListener("click", ()=>{
+    content.textContent = "";
+    content.appendChild(home());
+});
+
+menuTab.addEventListener("click", ()=>{
+    content.textContent = "";
+    content.appendChild(menu());
+});
+
+contactTab.addEventListener("click", ()=>{
+    content.textContent = "";
+    content.appendChild(contact());
+});
+
+
